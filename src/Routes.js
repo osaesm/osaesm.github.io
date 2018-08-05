3349f9290883cabf885e567c3dfb37ad4c80e2d9
Osama <osamabinesmail@gmail.com>
2018-08-05 16:27:51 -0500

    updated navbar

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import './Routes.css'
 import Home from './Home'
 import ContactMe from './ContactMe'
 
@@ -8,12 +9,17 @@ class Routes extends Component {
     return (
       <Router>
         <div>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/contact-me'>Contact Me</Link>
-          </li>
+          <div className='nav-bar'>
+            <div className='link'>
+              <Link to='/'>Home</Link>
+            </div>
+            <div className='link'>
+              <a target='_blank' without rel="noopener noreferrer" href='https://docs.google.com/viewer?url=https://github.com/osaesm/osaesm.github.io/raw/master/Resume.pdf'>Resume</a>
+            </div>
+            <div className='link'>
+              <Link to='/contact-me'>Contact Me</Link>
+            </div>
+          </div>
           <Route exact path='/' component={Home} />
           <Route path='/contact-me' component={ContactMe} />
         </div>

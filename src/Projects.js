0b2fb814bda426f9cc33968e5ea506c3f873f5a5
osaesm <osamabinesmail@gmail.com>
2018-09-09 17:45:40 -0500

    trimmed css and redid footer

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Projects.css';
 
 class Projects extends Component {
   render() {
--- src/Website.js
+++ src/Website.js
@@ -13,16 +13,15 @@ class Website extends Component {
           <Routes />
         </div>
         <div className='footer'>
-          {/* <hr classname='horizontal-break' /> */}
-          <p className='first-line'>
-            Email: <a href={`mailto:${email}`}>{email}</a> |
-            Phone: <a href='tel:+18478341796'>{phoneNumber}</a> |
-            Mailing Address: <a href='https://goo.gl/maps/9orw4vZDeFx' target='_blank' rel="noopener noreferrer" >{mailingAddress}</a>
-          </p>
-          <p>
-            LinkedIn: <a href={'https://www.linkedin.com/in/osamabinesmail/'} target='_blank' rel="noopener noreferrer" >/in/osamabinesmail</a> |
-            GitHub: <a href={'http://github.com/osaesm'} target='_blank' rel="noopener noreferrer" >/osaesm</a>
-          </p>
+          <div className='first-line'>
+            <strong>Email: </strong><a href={`mailto:${email}`}>{email}</a>
+            <strong> | Phone: </strong><a href='tel:+18478341796'>{phoneNumber}</a>
+            <strong> | Mailing Address: </strong><a href='https://goo.gl/maps/9orw4vZDeFx' target='_blank' rel="noopener noreferrer" >{mailingAddress}</a>
+          </div>
+          <div className='second-line'>
+            <strong>LinkedIn: </strong><a href={'https://www.linkedin.com/in/osamabinesmail/'} target='_blank' rel="noopener noreferrer" >/in/osamabinesmail</a>
+            <strong> | GitHub: </strong><a href={'http://github.com/osaesm'} target='_blank' rel="noopener noreferrer" >/osaesm</a>
+          </div>
         </div>
       </div>
     );

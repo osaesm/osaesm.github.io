3714d65a2b86985fececb12bb745191412353a4a
osaesm <osamabinesmail@gmail.com>
2018-09-10 15:23:34 -0500

    added cards

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component {
           <h1 className="display-3">Osama Esmail</h1>
           <figure>
             <img className='circle-pic' alt='Pic of me should have loaded' src='https://github.com/osaesm/osaesm.github.io/raw/master/Me.jpg' />
-            <figcaption>I look more happy in person (I just don't smile for cameras)</figcaption>
+            <figcaption className='caption'>I look more happy in person (I just don't smile for cameras)</figcaption>
           </figure>
           <div className="lead">
             I'm a sophomore at the University of Illinois at Urbana-Champaign studying computer science.
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import './Projects.css';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 class Projects extends Component {
   render() {
     return (
-      <div>
-        Placeholder. Going to add most recent projects here
+      <div className='card-collection'>
+        <Card className='leftmost'>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card className='rightmost'>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
       </div>
     );
   }

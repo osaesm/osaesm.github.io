081aca569a623ca3c67f305af37bfaad4b8f023f
Osama Esmail <osamabinesmail@gmail.com>
2021-04-15 21:52:02 -0500

    updated picture/resume/pages; reformatted image css; fixed package.json / package-lock.json issues I think

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,7 @@ class Home extends Component {
         <Container fluid className='center-everything'>
           <h1 className="display-3">Osama Esmail</h1>
           <figure>
-            <img className='circle-pic' alt='Pic of me should have loaded' src='Me.jpg' />
-            {/* <img className='circle-pic' alt='Pic of me should have loaded' src='https://github.com/osaesm/osaesm.github.io/raw/master/Me.jpg' /> */}
+            <img className='circle-pic' alt='Me in front of a blossoming tree' src='Me.jpg' />
           </figure>
           <div className="lead">
             I'm a senior at the University of Illinois at Urbana-Champaign studying computer science.
@@ -19,7 +18,7 @@ class Home extends Component {
             I also study math as a second major and keep tabs on new, interesting technologies.
           </div>
           <div className="lead">
-          I also enjoy following professional sports (especially NBA, NFL, and MLB), building various objects (custom PC, wood table), and gaming (lately big on WatchDogs).
+            I also enjoy following professional sports (especially NBA, NFL, and MLB), building various objects (custom PC, wood table), and gaming (lately big on The Witcher 3).
           </div>
           <div className="lead">
             This website is somewhat a WIP, so there will be some changes and things moved around. Welcome to my personal website!

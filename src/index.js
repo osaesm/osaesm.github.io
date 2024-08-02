c269033cf1740644535321563a94a0aad949bdb0
Osama <osamabinesmail@gmail.com>
2024-08-02 11:41:54 -0700

    simpler website

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Website from './Website';
-
-ReactDOM.render(<Website />, document.getElementById('root'));
-registerServiceWorker();
